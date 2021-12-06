0863fc9f7e2a9e568035f0ad9a0a19394e2cabba
Brandon Plaza <brandonplaza6@gmail.com>
2021-12-05 19:55:44 -0500

    Fixed merge

--- API/Model/Persistence/DataContext.cs
+++ API/Model/Persistence/DataContext.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Model.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Model.Persistence
 {
-    public class DataContext
+    public class DataContext : DbContext
     {
         public DbSet<User> Users { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
