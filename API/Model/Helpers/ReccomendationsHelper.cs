93900b3740cacab85b2dc2cbb3502ac9cdf3368a
Brandon Plaza <brandonplaza6@gmail.com>
2021-12-07 00:05:28 -0500

    Replaced characters in string response from API

--- API/Model/Helpers/ReccomendationsHelper.cs
+++ API/Model/Helpers/ReccomendationsHelper.cs
@@ -8,7 +8,7 @@ namespace API.Model.Helpers
 {
     public static class ReccomendationsHelper
     {
-       public static string GetAllMediaByTitle(string mediaTitle){
+       public static IEnumerable<string> GetAllMediaByTitle(string mediaTitle){
            string apiKey = "6bfc5edb70cf2f059fcfc2b4517f89dd";
            var client = new RestClient("https://api.themoviedb.org/3/search/multi");
            client.Timeout = -1;
@@ -16,8 +16,10 @@ namespace API.Model.Helpers
            request.AddParameter("api_key",apiKey);
            request.AddParameter("query",mediaTitle);
            IRestResponse response = client.Execute(request);
-           string rawResponse = response.Content;
-           return rawResponse;
+           string[] rawResponse = response.Content.Split(new string[] {","}, StringSplitOptions.RemoveEmptyEntries);
+           foreach(string responseString in rawResponse){
+               yield return responseString.Replace("\"", string.Empty);
+           }
        }
     }
 }
