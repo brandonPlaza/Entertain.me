7f3ff13ad16a23e4ae57156db819314a670f8177
Brandon Plaza <brandonplaza6@gmail.com>
2021-12-08 17:03:52 -0500

    Added JSON properties to Media

--- API/Model/Entities/Media.cs
+++ API/Model/Entities/Media.cs
@@ -1,12 +1,21 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace API.Model.Entities
 {
     public class Media
     {
-        public Guid Id { get; set; }
+        [Key]
+        [JsonPropertyName("id")] public int Id { get; set; } //This must be an int because the API has their Ids as ints
+        [JsonPropertyName("adult")] public bool Adult { get; set; }
+        [JsonPropertyName("genre_ids")] public List<int> GenreIds { get; set; } = new List<int>();
+        [JsonPropertyName("media_type")] public string MediaType { get; set; } = "";
+        [JsonPropertyName("original_language")] public string Language { get; set; } = "";
+        [JsonPropertyName("title")] public string Title { get; set; } = "";
+        [JsonPropertyName("overview")] public string Overview { get; set; } = "";
     }
 }
