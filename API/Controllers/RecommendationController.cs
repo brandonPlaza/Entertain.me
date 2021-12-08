be7c5cb205f4266aa76ddb2398b57eb08fd62685
Ethan McTague <ethan@tague.me>
2021-12-08 17:37:49 -0500

    Add favouriting

--- API/Controllers/RecommendationController.cs
+++ API/Controllers/RecommendationController.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using API.Model.Entities;
 using API.Model.Helpers;
 using API.Model.Persistence;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -12,11 +15,14 @@ namespace API.Controllers
     {
         private MovieApiHelper _movieHelper;
         private DataContext _db;
+        private readonly UserManager<User> _userManager;
 
-        public RecommendationController(MovieApiHelper movieHelper, DataContext db)
+
+        public RecommendationController(MovieApiHelper movieHelper, DataContext db, UserManager<User> manager)
         {
             _movieHelper = movieHelper;
             _db = db;
+            _userManager = manager;
         }
 
         [HttpGet]
@@ -24,6 +30,15 @@ namespace API.Controllers
         {
             var results = await _movieHelper.RecommendMovies(_db, movieIDs);
             return Ok(results);
+        }
+
+        [Authorize]
+        [HttpGet]
+        [Route("api/[controller]/[action]")]
+        public async Task FavouriteMovie([FromQuery] int movieId)
+        {
+            (await _userManager.GetUserAsync(HttpContext.User)).Favourites
+                .Add(await _db.Media.FindAsync(new {Id = movieId}));
         }
     }
 }
