e0e273ff4c7d71c64af2dbc898d870e2239f2743
Brandon Plaza <brandonplaza6@gmail.com>
2021-12-08 17:28:15 -0500

    Updated SearchHelper class

--- API/Controllers/SearchController.cs
+++ API/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using API.Model.Entities;
 using API.Model.Helpers;
+using API.Model.Persistence;
 using API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -17,17 +18,19 @@ namespace API.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly TokenService _tokenService;
+        private readonly DataContext _context;
 
-        public SearchController(UserManager<User> userManager, SignInManager<User> signInManager, TokenService tokenService)
+        public SearchController(UserManager<User> userManager, SignInManager<User> signInManager, TokenService tokenService, DataContext context)
         {
             this._userManager = userManager;
             this._signInManager = signInManager;
             this._tokenService = tokenService;
+            this._context = context;
         }
         [HttpGet]
         public async Task<IActionResult> GetMediaByTitle([FromQuery] string mediaTitle)
         {
-            var response = RecommendationsHelper.GetAllMediaByTitle(mediaTitle);
+            var response = SearchHelper.SearchForMedia(_context,mediaTitle);
             return Ok(response);
         }
     }
