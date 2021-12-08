5a4fb51812b63a663b36ac3af9f6cb8b0a4cdf67
Ethan McTague <ethan@tague.me>
2021-12-08 17:23:58 -0500

    Remove redundant search tool

--- API/Model/Helpers/MovieApiHelper.cs
+++ API/Model/Helpers/MovieApiHelper.cs
@@ -44,24 +44,6 @@ namespace API.Model.Helpers
             await db.Media.AddRangeAsync(unique);
             return unique;
         }
-
-        /// Search for movies based on a title.
-        public async Task<List<Media>> SearchForMovies(DataContext db, string title)
-        {
-            var data = await _client.GetAsync($"https://api.themoviedb.org/3/search/multi?api_key={_key}&query=" +
-                             UrlEncoder.Default.Encode(title));
-            if (!data.IsSuccessStatusCode)
-                throw new HttpRequestException("API Failure: " + await data.Content.ReadAsStringAsync());
-
-            var result = await data.Content.ReadFromJsonAsync<MovieSearchResults>();
-            
-            if (result == null)
-                throw new HttpRequestException("Failed to parse API result");
-
-            await db.Media.AddRangeAsync(result.Results);
-
-            return result!.Results;
-        }
         
         private class MovieSearchResults
         {
